b459fde0df79e2a9867395043cb8ff370922de71
pfrnands <pdrofrnands@gmail.com>
2025-12-13 23:19:19 -0300

    13/12

---
+++ estrutura_de_dados/listas.py
@@ -0,0 +1,7 @@
+['laranja', 'maca', 'uva', 'banana']
+[]
+['p', 'y','t', 'h', 'o', 'n']
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+['Ferrari', 'F8',4200000, 2020, 2900, 'São Paulo', True]
+# Exemplos de listas em Python
+
